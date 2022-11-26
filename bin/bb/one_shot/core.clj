9c60d82e24e7b87e03284e0aaa4d26895b507535
Ben Allred <skuttleman@gmail.com>
2022-11-26 01:11:47 -0700

    initial commit

---
+++ bin/bb/one_shot/core.clj
@@ -0,0 +1,18 @@
+(ns one-shot.core
+  (:require
+    [babashka.process :as p]))
+
+(def ^:const ^:private SYNC_PUSH_COMMAND
+  "aws s3 sync Assets/Scenes/resources s3://one-shot")
+
+(def ^:const ^:private SYNC_PULL_COMMAND
+  "aws s3 sync s3://one-shot Assets/Scenes/resources")
+
+(defn ^:private sh [command]
+  (p/process ["sh" "-c" command]))
+
+(defn -main [& args]
+  (case (vec args)
+    ["s3" "push"] (sh SYNC_PUSH_COMMAND)
+    ["s3" "pull"] (sh SYNC_PULL_COMMAND)
+    ["fuck" "yourself"] (println "... I die a little every time. \uD83D\uDE14")))
