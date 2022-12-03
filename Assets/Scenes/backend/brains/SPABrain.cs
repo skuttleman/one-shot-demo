b6e19c7841d8bbec93dee65fa302ea3572f43b49
Ben Allred <skuttleman@gmail.com>
2022-12-03 04:14:03 -0700

    back to physics movement

--- Assets/Scenes/backend/brains/SPABrain.cs
+++ Assets/Scenes/backend/brains/SPABrain.cs
@@ -9,11 +9,13 @@ using UnityEngine;
 using static OSCore.Events.Brains.SPA.SPAEvent;
 
 namespace OSBE {
-    public class SPABrain : AControllerBrain<MoveSPA, FaceSPA, InitEvent<GravityCfgSO>, InitEvent<CollisionCfgSO>> {
+    public class SPABrain : AControllerBrain
+        <MoveSPA, FaceSPA, InitEvent<GravityCfgSO>, InitEvent<CollisionCfgSO>> {
         readonly IGameSystem system;
         readonly Transform target;
         GravityCfgSO gravity;
         CollisionCfgSO collision;
+        Rigidbody rb = null;
 
         public SPABrain(IGameSystem system, Transform target) {
             this.system = system;
@@ -21,8 +23,13 @@ namespace OSBE {
         }
 
         public override void Handle(MoveSPA e) {
+            if (rb is null)
+                rb = target.GetComponent<Rigidbody>();
+
+            Vector3 dir = e.speed * Time.fixedDeltaTime * Vectors.Upgrade(e.direction);
+
             if (Vectors.NonZero(e.direction))
-                target.position += e.speed * Time.deltaTime * Vectors.Upgrade(e.direction);
+                rb.AddForce(dir);
         }
 
         public override void Handle(FaceSPA e) {
