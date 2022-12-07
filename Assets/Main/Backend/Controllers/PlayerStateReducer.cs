8d25de2c63ff159a23da9a28d9f0c7328cc21a5b
Ben Allred <skuttleman@gmail.com>
2022-12-07 06:28:57 -0700

    quick follow refactor

--- Assets/Main/Backend/Controllers/PlayerStateReducer.cs
+++ Assets/Main/Backend/Controllers/PlayerStateReducer.cs
@@ -20,10 +20,11 @@ namespace OSBE.Controllers {
 
         readonly IGameSystem system;
         readonly Transform target;
+        readonly Animator anim;
+
         IStateReceiver<PlayerState> receiver;
         PlayerCfgSO cfg = null;
         PlayerState state;
-        readonly Animator anim;
         GameObject stand;
         GameObject crouch;
         GameObject crawl;
@@ -48,7 +49,7 @@ namespace OSBE.Controllers {
             crouch = GameObject.Find("/Characters/Player/Entity/crouch");
             crawl = GameObject.Find("/Characters/Player/Entity/crawl");
 
-            state = new PlayerState {
+            EmitState(new PlayerState {
                 movement = Vector2.zero,
                 facing = Vector2.zero,
                 stance = PlayerStance.STANDING,
@@ -57,9 +58,7 @@ namespace OSBE.Controllers {
                 isMoving = false,
                 isSprinting = false,
                 isScoping = false
-            };
-
-            receiver.OnStateChange(state);
+            });
             ActivateStance();
         }
 
@@ -67,32 +66,29 @@ namespace OSBE.Controllers {
             bool isMoving = Vectors.NonZero(direction);
             anim.SetBool(ANIM_MOVE, isMoving);
 
-            state = state with {
+            EmitState(state with {
                 isMoving = isMoving,
                 movement = direction,
                 isSprinting = isMoving && state.isSprinting
-            };
-            receiver.OnStateChange(state);
+            });
         }
 
         public void OnSprintInput(bool isSprinting) {
             if (isSprinting && ShouldTransitionToSprint()) {
                 anim.SetInteger(ANIM_STANCE, (int)PlayerStance.STANDING);
-                state = state with {
+                EmitState(state with {
                     stance = PlayerStance.STANDING,
                     isSprinting = true
-                };
-                receiver.OnStateChange(state);
+                });
             }
         }
 
         public void OnLookInput(Vector2 direction, bool isMouse) {
-            state = state with {
+            EmitState(state with {
                 facing = direction,
                 isSprinting = false,
                 mouseLookTimer = isMouse && Vectors.NonZero(direction) ? cfg.mouseLookReset : state.mouseLookTimer
-            };
-            receiver.OnStateChange(state);
+            });
         }
 
         public void OnStanceInput(float holdDuration) {
@@ -103,14 +99,12 @@ namespace OSBE.Controllers {
             if (!state.isMoving || PCUtils.IsMovable(nextStance, state))
                 anim.SetInteger(ANIM_STANCE, (int)nextStance);
 
-            state = state with { isSprinting = false };
-            receiver.OnStateChange(state);
+            EmitState(state with { isSprinting = false });
         }
 
         public void OnAimInput(bool isAiming) {
             anim.SetBool(ANIM_AIM, isAiming);
-            state = state with { isSprinting = false };
-            receiver.OnStateChange(state);
+            EmitState(state with { isSprinting = false });
         }
 
         public void OnAttackInput(bool isAttacking) {
@@ -126,47 +120,41 @@ namespace OSBE.Controllers {
                         .Then(() => anim.SetBool(ANIM_ATTACK, false));
                 });
 
-                state = state with { isSprinting = false };
-                receiver.OnStateChange(state);
+                EmitState(state with { isSprinting = false });
             }
         }
 
         public void OnScopeInput(bool isScoping) {
             anim.SetBool(ANIM_SCOPE, isScoping);
 
-            state = state with { isSprinting = !isScoping && state.isScoping };
-            receiver.OnStateChange(state);
+            EmitState(state with { isSprinting = !isScoping && state.isScoping });
         }
 
         public void OnStanceChanged(PlayerStance stance) {
             if (state.stance != stance) {
                 ActivateStance();
-                state = state with { stance = stance };
-                receiver.OnStateChange(state);
+                EmitState(state with { stance = stance });
                 PublishMessage(new StanceChanged(stance));
             }
         }
 
         public void OnAttackModeChanged(AttackMode attackMode) {
             if (state.attackMode != attackMode) {
-                state = state with { attackMode = attackMode };
-                receiver.OnStateChange(state);
+                EmitState(state with { attackMode = attackMode });
                 PublishMessage(new AttackModeChanged(attackMode));
             }
         }
 
         public void OnMovementChanged(bool isMoving) {
             if (state.isMoving != isMoving) {
-                state = state with { isMoving = isMoving };
-                receiver.OnStateChange(state);
+                EmitState(state with { isMoving = isMoving });
                 PublishMessage(new MovementChanged(isMoving));
             }
         }
 
         public void OnScopingChanged(bool isScoping) {
             if (state.isScoping != isScoping) {
-                state = state with { isScoping = isScoping };
-                receiver.OnStateChange(state);
+                EmitState(state with { isScoping = isScoping });
                 PublishMessage(new ScopingChanged(isScoping));
             }
         }
@@ -176,9 +164,6 @@ namespace OSBE.Controllers {
         bool ShouldTransitionToSprint() =>
                 !state.isSprinting && !state.isScoping && !PCUtils.IsAiming(state.attackMode);
 
-        public void Init(PlayerCfgSO cfg) =>
-            this.cfg = cfg;
-
         void PublishMessage(IEvent message) =>
             system.Send<IPubSub>(pubsub => pubsub.Publish(message));
 
@@ -186,6 +171,11 @@ namespace OSBE.Controllers {
             stand.SetActive(state.stance == PlayerStance.STANDING);
             crouch.SetActive(state.stance == PlayerStance.CROUCHING);
             crawl.SetActive(state.stance == PlayerStance.CRAWLING);
+        }
+
+        void EmitState(PlayerState state) {
+            this.state = state;
+            receiver.OnStateChange(state);
         }
     }
 
